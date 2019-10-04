612ef9ac3ab3fb638ab07db0b085613e73e7b5e8
Stijn Nuiten <stijnnuiten@gmail.com>
2019-10-04 12:54:18 +0200

    Updated label_diff to allow multiple labels p/plot

--- plotfuncs.py
+++ plotfuncs.py
@@ -15,18 +15,22 @@ from math import *
 from IPython import embed as shell
 from itertools import combinations
 
-def label_diff(ax,means,stds,txt,mark = True):
+def label_diff(ax,means,stds,txt,width=None,pos=0, mark = True):
 	# This method draws significans boxes in bar-plots. Up until now this works for barplots with 3 bars (i.e. 3 significance boxes).
 	# Inputs are the handle of a (sub-)figure, the mean values and errors of the barplot, and lastly the actual text labels in a list.
 	# TO ADD:
 	# - exclude specific comparissons
 	# - more than 3 bars
+	# shell()
 	means=np.array(means)
 	stds = np.array(stds) 
-	lims = means + stds
+	# lims = means + stds
 	num_bars = len(means)
 	num_diff = factorial(num_bars)/2
-	h = np.max(lims)*2*0.1
+	h =  np.diff(ax.get_ylim())[0] *0.05
+
+	# abs(np.array(means)).max()*0.25 
+
 
 	if num_diff > 1:
 		lines = [abs(a - b) for a, b in combinations(range(num_diff), 2)]
@@ -34,7 +38,7 @@ def label_diff(ax,means,stds,txt,mark = True):
 		lines = [1]
 	bar = 0
 
-	for d in range(num_diff):
+	for d in range(int(num_diff)):
 		if mark:
 			if float(txt[0].split(' ')[-1]) < 0.05:
 				txt[d] = '*'
@@ -45,20 +49,37 @@ def label_diff(ax,means,stds,txt,mark = True):
 
 		if lines[d]==1 and d>0:
 			bar =+ 1
-		x = [bar, bar + lines[d]]
+		# shell()
+		if width:
+			x =[bar-width, bar+width]
+		else:
+			x = [bar, bar + lines[d]]
 
-		high=np.array(means[x[0]:x[1]+1])+np.array(stds[x[0]:x[1]+1])
-		low=np.array(means[x[0]:x[1]+1])-np.array(stds[x[0]:x[1]+1])		
+		x = [x[0] + pos , x[1] +pos]
+		high=means + stds
+		low=means - stds		
+		# shell()
 		maxdat = np.max((high,low))
 		mindat = np.min((high,low))
+		maxval = np.max([abs(mindat),abs(maxdat)])
+		y_box = maxval + h
+		y_text = y_box + 3* h
+		# h = [lim*1.5 for lim in lims]
+		# shell()
 		if abs(mindat) > abs(maxdat):
-			y = mindat -  h - ((lines[d]-1) * 0.2 )
+			if mindat < 0 :
+				y_box = -y_box 
+				y_text = -y_text
+				h = -h
+
 		else:
-			y = maxdat +  h + ((lines[d]-1) * 0.2 )
-
-		ax.plot([x[0], x[0], x[1], x[1]], [y, y+h, y+h, y], lw=1.5, color='k')
-		ax.text((x[0]+x[1])*.5, y+h, txt[d], ha='center', va='bottom')
-
+			if maxdat < 0:
+				y_box = -y_box 
+				y_text = -y_text
+				h=-h
+		# shell()
+		ax.plot([x[0], x[0], x[1], x[1]], [y_box, y_box+h, y_box+h, y_box], lw=1.5, color='k')
+		ax.text((x[0]+x[1])*.5, y_text, txt[d], ha='center', va='center')
 	return ax
 
 
